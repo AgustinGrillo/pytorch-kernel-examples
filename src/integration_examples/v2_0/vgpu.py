1178878cdd3c480b6851c19b1b589c49033e34be
Agustin Grillo <grilloagus@gmail.com>
2024-05-20 18:27:30 -0300

    Generate vgpu methods

--- src/integration_examples/v2_0/vgpu.py
+++ src/integration_examples/v2_0/vgpu.py
@@ -5,3 +5,7 @@ torch.ops.load_library("build/libcustom_backend.so")
 
 # Define custom device name
 torch.utils.rename_privateuse1_backend("vgpu")
+unsupported_dtype = []
+torch.utils.generate_methods_for_privateuse1_backend(
+    for_tensor=True, for_module=True, for_storage=True
+)
