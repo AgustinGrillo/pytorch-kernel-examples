6fbacaec6aa922a6d28e17477e321a89e2ecd547
Agustin Grillo <grilloagus@gmail.com>
2024-05-27 19:29:30 -0300

    Update benchmarking

--- src/custom_ops/mmult/benchmark.py
+++ src/custom_ops/mmult/benchmark.py
@@ -1,12 +1,15 @@
 import torch
 import vgpu
+import torch.utils.benchmark as benchmark
+from torch.profiler import profile, schedule, record_function, ProfilerActivity
 import time
+import numpy as np
 
-N = 100
+N = 10
 
-m = 1000
-k = 1000
-n = 1000
+m = int(3000)
+k = int(3000)
+n = int(3000)
 
 
 # Create two random matrices in float32
@@ -15,7 +18,7 @@ b = torch.randn(k, n)
 
 operators = [
     torch.mm,
-    torch.vgpu.ops.mmult_passthrough,
+    # torch.vgpu.ops.mmult_passthrough,
     # torch.vgpu.ops.mmult_naive,
     # torch.vgpu.ops.mmult_naive_multithreaded,
     torch.vgpu.ops.mmult_mkl,
@@ -23,19 +26,58 @@ operators = [
 
 # torch.vgpu.utils.set_num_threads(4)
 
-# Warm up
-print("Warming up...")
-for i in range(N):
-    start_time = time.time()
-    torch.mm(a, b)
-
 # Benchmark
 for operator in operators:
-    operator(a, b)  # Warm up
-    start_time = time.time()
+    print(f"[{operator.__module__}.{operator.__name__}] Warming up...")
     for i in range(N):
+        operator(a, b)  # Warm up
+
+    start_time = None
+    times = np.array([])
+    for i in range(N):
+        start_time = time.perf_counter()
         operator(a, b)
-    total_time = time.time() - start_time
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        times = np.append(times, total_time)
+
+    print("Raw results:")
     print(
-        f"[{operator.__module__}.{operator.__name__}] Average time per matrix multiplication (µs): { 1e6 * total_time / N }"
+        f"[{operator.__module__}.{operator.__name__}] Average time per matrix multiplication (µs): { 1e6 * np.mean(times):.2f}"
     )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Median time per matrix multiplication (µs): { 1e6 * np.median(times):.2f}"
+    )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Standard deviation of time per matrix multiplication (µs): { 1e6 * np.std(times):.2f}"
+    )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Min / Max time per matrix multiplication (µs): { 1e6 * np.min(times):.2f} / { 1e6 * np.max(times):.2f}"
+    )
+    print("Results without outliers:")
+    times = times[times < np.mean(times) + 2 * np.std(times)]
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Average time per matrix multiplication (µs): { 1e6 * np.mean(times):.2f}"
+    )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Median time per matrix multiplication (µs): { 1e6 * np.median(times):.2f}"
+    )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Standard deviation of time per matrix multiplication (µs): { 1e6 * np.std(times):.2f}"
+    )
+    print(
+        f"[{operator.__module__}.{operator.__name__}] Min / Max time per matrix multiplication (µs): { 1e6 * np.min(times):.2f} / { 1e6 * np.max(times):.2f}"
+    )
+
+
+# for operator in operators:
+#     for i in range(N):
+#         operator(a, b)  # Warm up
+#     with profile(
+#         activities=[ProfilerActivity.CPU],
+#     ) as prof:
+#         for i in range(N):
+#             with record_function("operator_call"):
+#                 operator(a, b)
+#
+#     print(prof.key_averages())
